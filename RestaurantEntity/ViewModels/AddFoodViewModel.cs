a33a9bd6091edbce7c07850d4d11c21743a37558
yandutka@mail.ru <yandutka@mail.ru>
2022-07-01 21:14:07 +0300

    commit

---
+++ RestaurantEntity/ViewModels/AddFoodViewModel.cs
@@ -0,0 +1,58 @@
+﻿using RestaurantEntity.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RestaurantEntity.ViewModels
+{
+    class AddFoodViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        Menu _menu;
+        UserContext _context;
+
+        public AddFoodViewModel()
+        {
+            _menu = new Menu();
+        }
+
+        public Menu AddMenu
+        {
+            get { return _menu; }
+            set
+            {
+                _menu = value;
+                Notify("AddMenu");
+            }
+        }
+
+        public Commands AddButtonClick
+        {
+            get
+            {
+                return new Commands(
+                    new Action<object>((obj) =>
+                    {
+                        _context = new UserContext();
+                        _context.RestaurantMenu.Add(_menu);
+                        _context.SaveChanges();
+                        Window window = (Window)obj;
+                        window.Close();
+                    }),
+                    new Func<bool>(() =>
+                    {
+                        return _menu.Name != "" && _menu.Photo != "" && _menu.Price != 0;
+                    }));
+            }
+        }
+    }
+}
--- RestaurantEntity/ViewModels/MenuViewModel.cs
+++ RestaurantEntity/ViewModels/MenuViewModel.cs
@@ -13,9 +13,16 @@ namespace RestaurantEntity.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        //В MVVM нельзя использовать чистый List, поэтому:
+        //В MVVM не стоит использовать чистый List при биндинге, поэтому:
         ObservableCollection<string> _categories;
+        ObservableCollection<string> _menuNames;
+        List<Menu> _menu;
         UserContext _context;
+        string _selectedCategory;
+        int _selectedId;
+        string _name;
+        int _price;
+        string _url;
 
         void Notify(string name)
         {
@@ -25,13 +32,17 @@ namespace RestaurantEntity.ViewModels
         {
             _context = new UserContext();
             _categories = new ObservableCollection<string>();
+            _menuNames = new ObservableCollection<string>();
+            _menu = new List<Menu>();
+
+            Categories.Add("нет");
             foreach(Category category in _context.Categories)
             {
                 Categories.Add(category.Name);
             }
+            SelectedCategory = "нет";
             Notify("Categories");
         }
-
         public ObservableCollection<string> Categories
         {
             get { return _categories; }
@@ -39,6 +50,97 @@ namespace RestaurantEntity.ViewModels
             {
                 _categories = value;
                 Notify("Categories");
+            }
+        }
+        public ObservableCollection<string> MenuNames
+        {
+            get { return _menuNames; }
+            set
+            {
+                _menuNames = value;
+                Notify("MenuNames");
+            }
+        }
+        public string SelectedCategory
+        {
+            get { return _selectedCategory; }
+            set
+            {
+                _selectedCategory = value;
+                if (_selectedCategory != "нет")
+                {
+                    Category category = (from i in _context.Categories
+                                         where i.Name == _selectedCategory
+                                         select i).FirstOrDefault();
+                    _menu = (from x in _context.RestaurantMenu
+                             where x.Category.Id == category.Id
+                             select x).ToList();
+                }
+                else 
+                    _menu = _context.RestaurantMenu.ToList();
+                
+                SelectedId = 0;
+                MenuNames.Clear();
+                foreach(Menu menu in _menu)
+                {
+                    _menuNames.Add(menu.Name);
+                }
+                Notify("SelectedCategory");
+            }
+        }
+        public int SelectedId
+        {
+            get { return _selectedId; }
+            set
+            {
+                _selectedId = value;
+                if (_selectedId != -1)
+                {
+                    Name = _menu[_selectedId].Name;
+                    Price = _menu[_selectedId].Price;
+                    Url = _menu[_selectedId].Photo; 
+                }
+                Notify("SelectedId");
+            }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                Notify("Name");
+            }
+        }
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                Notify("Price");
+            }
+        }
+        public string Url
+        {
+            get { return _url; }
+            set
+            {
+                _url = value;
+                Notify("Url");
+            }
+        }
+
+        public Commands AddClick
+        {
+            get 
+            {
+                return new Commands(new Action(() =>
+                {
+                    AddFood addFood = new AddFood();
+                    addFood.ShowDialog();
+                    _selectedCategory = "нет";
+                }));
             }
         }
     }
